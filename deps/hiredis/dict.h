13594778f1dabf6aea8c66808ffb56871b829cdb
liulijuan <liulijuan132@qq.com>
2021-01-06 22:29:55 +0800

    redis db

--- deps/hiredis/dict.h
+++ deps/hiredis/dict.h
@@ -58,10 +58,15 @@ typedef struct dictType {
 } dictType;
 
 typedef struct dict {
+    // 哈希表数组
     dictEntry **table;
+    // 哈希表类型
     dictType *type;
+    // 哈希表大小
     unsigned long size;
+    // 哈希表大小掩码，用于计算索引值，总是等于size-1
     unsigned long sizemask;
+    // 该哈希表已有节点的数量
     unsigned long used;
     void *privdata;
 } dict;
